98eb26dfb125ea10b1f8670645f8d62b8da3cee6
sindhuprya <sindhuprya24@gmail.com>
2026-01-30 13:13:49 +0530

    Add files via upload

---
+++ 4task2.py
@@ -0,0 +1,14 @@
+import math
+
+# Ask the user for input
+num = float(input("Enter a number: "))
+
+# Calculations using math module
+square_root = math.sqrt(num)
+natural_log = math.log(num)
+sine_value = math.sin(num)
+
+# Display results
+print(f"Square root of {num} is: {square_root}")
+print(f"Natural logarithm of {num} is: {natural_log}")
+print(f"Sine of {num} (in radians) is: {sine_value}")
